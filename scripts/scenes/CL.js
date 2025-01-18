b0f39c949dfa737a73f341a591641fb51fdbb773
猪猪王笨伟 <65105093+PiGPiG2048@users.noreply.github.com>
2025-01-19 06:16:54 +0800

    Add files via upload

---
+++ scripts/scenes/CL.js
@@ -0,0 +1,15 @@
+import {view, ctx, clickPos, loadScene} from "../main.js"
+import { paintButton } from "../ui/Components.js"
+import { menu } from "./menu.js"
+import {testmap} from "../Map.js"
+
+export let CL = {
+	load: function() {
+		paintButton(25,25,50,100,"rgba(255,255,255,255)", () => {
+			loadScene(menu)
+		})
+		paintButton(100,100,100,100,"rgba(255,255,255,255)",() => {
+			loadScene(testmap)
+		})
+	}
+}
