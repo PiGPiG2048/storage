fb2eff4db535a98b8387ef47c30cce133d042adc
猪猪王笨伟 <65105093+PiGPiG2048@users.noreply.github.com>
2025-01-19 00:46:59 +0800

    Update menu.js

--- scripts/scenes/menu.js
+++ scripts/scenes/menu.js
@@ -4,9 +4,9 @@ import { paintButton } from "./ui/Components.js"
 export let menu = {
 	load: function(){
 		clearScene()
-		paintButton(175,600,150,250,"rgba(200,200,200,255)"() => {})
-		paintButton(505,600,150,250,"rgba(200,200,200,255)"() => {})
-		paintButton(835,600,150,250,"rgba(200,200,200,255)"() => {
+		paintButton(175,600,150,250,"rgba(200,200,200,255)",() => {})
+		paintButton(505,600,150,250,"rgba(200,200,200,255)",() => {})
+		paintButton(835,600,150,250,"rgba(200,200,200,255)",() => {
 			window.opener = null;
 			window.open("","_self")
 			window.close()
