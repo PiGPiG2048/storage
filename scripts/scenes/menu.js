34375f9765ab146422395e4874f4b54cb4617b82
猪猪王笨伟 <65105093+PiGPiG2048@users.noreply.github.com>
2025-01-19 06:04:33 +0800

    Update menu.js

--- scripts/scenes/menu.js
+++ scripts/scenes/menu.js
@@ -1,13 +1,23 @@
-import {view, ctx, clickPos, clearScene} from "../main.js"
+import {view, ctx, clickPos, clearScene, loadScene} from "../main.js"
 import { paintButton } from "../ui/Components.js"
+import { CL } from "./CL.js"
+//import { setting } from "./setting.js"
 
 export let menu = {
 	load: function(){
 		clearScene()
-		paintButton(175,600,150,250,"rgba(200,200,200,255)",() => {})
-		paintButton(505,600,150,250,"rgba(200,200,200,255)",() => {})
-		paintButton(835,600,150,250,"rgba(200,200,200,255)",() => {
-			window.opener = null;
+		ctx.font = "80px serif"
+		ctx.fillText("设置",220,600)
+		paintButton(175,600,250,150,"rgba(200,200,200,255)",() => {
+			alert("少女施工中")
+		})
+		ctx.fillText("开始",550,600)
+		paintButton(505,600,250,150,"rgba(200,200,200,255)",() => {
+			loadScene(CL)
+		})
+		ctx.fillText("退出",645,600)
+		paintButton(835,600,250,150,"rgba(200,200,200,255)",() => {
+			window.opener = null
 			window.open("","_self")
 			window.close()
 		})
