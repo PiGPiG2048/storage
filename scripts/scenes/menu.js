f265f76491cac56613e62b75c2eb03a1781689fd
猪猪王笨伟 <65105093+PiGPiG2048@users.noreply.github.com>
2025-01-19 00:34:00 +0800

    Update menu.js

--- scripts/scenes/menu.js
+++ scripts/scenes/menu.js
@@ -2,7 +2,7 @@ import {view, ctx, clickPos} from "./main.js"
 import { paintButton } from "./ui/Components.js"
 
 export let menu = {
-	load: function()[
+	load: function(){
 		clearScene()
 		paintButton(175,600,150,250,"rgba(200,200,200,255)"() => {})
 		paintButton(505,600,150,250,"rgba(200,200,200,255)"() => {})
